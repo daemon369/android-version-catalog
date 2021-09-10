eeb263923b1523d4028af3e6f82741b73d6492b2
daemon369 <daemon336699@gmail.com>
2021-09-10 17:20:27 +0800

    add alias "core.ktx" to "androidX" version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,11 @@ enableFeaturePreview("VERSION_CATALOGS")
 dependencyResolutionManagement {
     versionCatalogs {
         create("androidX") {
+            version("core", "1.6.0")
+            version("lifecycle", "2.3.1")
+
             alias("core").to("androidx.core:core:1.6.0")
+            alias("core.ktx").to("androidx.core", "core-ktx").versionRef("core")
         }
     }
 }
