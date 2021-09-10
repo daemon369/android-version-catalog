1c203cb9626352eb93cccf6eb8c60fad75cfd2a1
daemon369 <daemon336699@gmail.com>
2021-09-10 17:15:19 +0800

    create version catalog: "androidX"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
 rootProject.name = "gradle-dependency-version-catalog"
 include(":demo")
 enableFeaturePreview("VERSION_CATALOGS")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("androidX") {
+            alias("core").to("androidx.core:core:1.6.0")
+        }
+    }
+}
