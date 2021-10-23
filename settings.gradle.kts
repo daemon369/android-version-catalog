37568e6447cc597e83c4d26070d1faba2676996c
daemon369 <daemon336699@gmail.com>
2021-10-23 20:33:38 +0800

    remove "androidX" catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,15 +9,5 @@ dependencyResolutionManagement {
         create("kotlin") {
             from(files("gradle/kotlin.versions.toml"))
         }
-
-        create("androidX") {
-            version("core", "1.6.0")
-            version("lifecycle", "2.3.1")
-
-            alias("core").to("androidx.core:core:1.6.0")
-            alias("core.ktx").to("androidx.core", "core-ktx").versionRef("core")
-
-            bundle("core.ktx", listOf("core", "core.ktx"))
-        }
     }
 }
