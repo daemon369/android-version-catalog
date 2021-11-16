4715374cc8e6f78e433e37344711cb0e456b7338
daemon369 <daemon336699@gmail.com>
2021-11-17 06:26:37 +0800

    use version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-//        classpath(libs.gradle)
-//        classpath(libs.kotlin.plugin)
-        classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath(libs.gradle)
+        classpath(libs.kotlin.plugin)
+//        classpath("com.android.tools.build:gradle:7.0.2")
+//        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     }
 }
 
