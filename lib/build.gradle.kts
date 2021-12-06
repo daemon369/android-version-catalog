091427e16aa2f70a6efa392aa9ecdb996d87c000
Daemon <daemon336699@gmail.com>
2021-12-06 13:28:35 +0800

    upgrade kotlin to 1.5.32

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 catalog {
     versionCatalog {
-        version("kotlin", "1.5.31")
+        version("kotlin", "1.5.32")
         version("kotlin-coroutines", "1.5.2")
         version("x-lifecycle", "2.4.0")
 
